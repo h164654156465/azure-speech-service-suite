0d643daa262e1e2245569c519adce90ac1b99ba1
h164654156465 <h164654156465@gmail.com>
2020-03-03 15:19:39 +0800

    Add new controller that handles text-to-speech request

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
 const rp = require('request-promise');
-const request = require('request');
-const xmlbuilder = require('xmlbuilder');
-const uuidv1 = require('uuid/v1');
 
 const get_index = (req, res, next) => {
     res.render('index');
@@ -25,48 +21,4 @@ const get_token = (req, res, next) => {
         });
 }
 
-const text_to_speech = (req, res, next) => {
-    let token = req.query.token;
-    let lang = req.query.lang;
-    let voice = req.query.voice;
-    let text = req.query.text;
-    let filename = `${uuidv1()}.mp3`;
-    let path = `public/${filename}`;
-    let stream = fs.createWriteStream(path);
-
-    // Create the SSML request.
-    let xml_body = xmlbuilder.create('speak')
-        .att('version', '1.0')
-        .att('xml:lang', lang)
-        .ele('voice')
-        .att('xml:lang', lang)
-        .att('name', voice)
-        .txt(text)
-        .end();
-    // Convert the XML into a string to send in the TTS request.
-    let body = xml_body.toString();
-
-    let options = {
-        method: 'POST',
-        baseUrl: 'https://eastasia.tts.speech.microsoft.com/',
-        url: 'cognitiveservices/v1',
-        headers: {
-            'Authorization': 'Bearer ' + token,
-            'cache-control': 'no-cache',
-            'User-Agent': 'MTCSpeechAPI',
-            'X-Microsoft-OutputFormat': 'audio-24khz-48kbitrate-mono-mp3',
-            'Content-Type': 'application/ssml+xml'
-        },
-        body: body
-    }
-
-    stream.on('finish', () => {
-        console.log('stream end');
-
-        res.send({ status: 'success', path: filename });
-    });
-
-    request(options).pipe(stream);
-}
-
-module.exports = { get_index, get_token, text_to_speech }
+module.exports = { get_index, get_token }
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,4 @@ router.get('/', index.get_index);
 /* GET Azure Speech Service access token */
 router.get('/token', index.get_token);
 
-/* GET Request to Azure Speech Service to do text to speech */
-router.get('/text-to-speech', index.text_to_speech);
-
 module.exports = router;
